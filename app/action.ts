ebf6f624ffe03b2977b4a1eb2cb794b130ea0fa5
kanugurajesh <245122733172@mvsrec.edu.in>
2023-11-29 14:50:21 +0530

    Adding parameters in serveractions

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
-const fetchAnime = async () => {
-    const response = await fetch("https://shikimori.one/api/animes");
+const fetchAnime = async (page:number) => {
+    const response = await fetch(`https://shikimori.one/api/animes?page${page}&limit=8&order=popularity`);
     const data = await response.json();
     console.log(data);
     return data;
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { data } from "./_data";
 import fetchAnime from "./action";
 
 async function Home() {
-  const data = await fetchAnime();
+  const data = await fetchAnime(1);
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
